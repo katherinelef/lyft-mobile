30115a51854f66c3355e64b4d577ee3ce29915e1
katherinelef <katherine.lef@hotmail.com>
2017-12-29 17:44:06 -0500

    añadiendo vista register

--- js/sing-up.js
+++ js/sing-up.js
@@ -32,9 +32,28 @@ var codigo = rand_code(caracteres, longitud);
  alert("Tu codigo: LAB-" + codigo)
  console.log(parseInt(codigo));
 
+localStorage.setItem("nombre",codigo);
+
  window.location.href = 'verify.html';
 
   });
 
+$("#code").keyup(function () {
 
+  if(parseInt($("#code").val()) === parseInt( localStorage.nombre)) {
+    $("#next2").removeAttr("disabled");
+    $("#next2").addClass("pink")
+  }
+
+  else if(parseInt($("#code").val()) < parseInt(localStorage.nombre) || parseInt($("#code").val()) > parseInt(localStorage.nombre) ){
+  $("#next2").attr( "disabled", "disabled" )
+  $("#next2").removeClass("pink")
+}
+});
+
+$("#next2").click(function () {
+
+  window.location.href = 'register.html';
+
+})
   });
